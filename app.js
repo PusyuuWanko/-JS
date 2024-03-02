b408e8144da68e30b9ecae1fe0067f1514cab654
プシューわんこ/ <121094209+PusyuuWanko@users.noreply.github.com>
2024-03-02 23:26:24 +0900

    add

---
+++ app.js
@@ -0,0 +1,5 @@
+const 日本語定数 = "プシュー";
+console.log(日本語定数);
+const エレメント = document.querySelector("content");
+エレメント.innerHTML = 日本語定数;
+エレメント.innerHTML += "<br><a href='http://21emon.wjg.jp' target='_blank'>プシューわんこ/の提供するプシューゲートウェイへ行く</a>";
