2645fc2550124d8b18ca240ae0c80ca9080d7610
プシューわんこ/ <121094209+PusyuuWanko@users.noreply.github.com>
2024-03-21 10:32:27 +0900

    関数の追加

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ console.log(日本語定数);
 const エレメント = document.querySelector("content");
 エレメント.innerHTML = 日本語定数;
 エレメント.innerHTML += "<br><a href='http://21emon.wjg.jp' target='_blank'>プシューわんこ/の提供するプシューゲートウェイへ行く</a>";
+
+function 何() {
+  エレメント.innerHTML = "関数くんをよんだ？";
+}
+setTimeout(何(), 5000);
